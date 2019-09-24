45517a62dc03d11f8808ba75c569ef490e68c75b
ren trigoso <rtrigoso@cbcnewmedia.com>
2019-09-24 14:03:08 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rtrigoso/tmt
+
+go 1.13
+
+require github.com/schollz/progressbar/v2 v2.13.2
---
+++ tmt.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/schollz/progressbar/v2"
+)
+
+var (
+	m = flag.Int("m", 25, "")
+	c = 0
+)
+
+var usage = ` Usage: tmt [options...]
+
+Options:
+	-m	Number of minutes to count. Default is 25 minutes.
+`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprint(usage))
+	}
+
+	flag.Parse()
+
+	end := *m * 60
+	bar := progressbar.New(end)
+
+	bar.RenderBlank()
+	for range time.Tick(1 * time.Second) {
+		if c > end {
+			break
+		}
+		bar.Add(1)
+		c++
+	}
+
+}
